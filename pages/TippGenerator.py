9866f30947fdd194589a6e4c1b8f58d09fc009bd
GamblingStar2025 <newspoolestate@gmail.com>
2025-04-16 03:02:22 +0200

    Update TippGenerator.py

--- pages/TippGenerator.py
+++ pages/TippGenerator.py
@@ -8,4 +8,3 @@ if not email:
 st.title("🎰 Tipp Generator")
 st.markdown("Hier kannst du deine Tipps basierend auf deiner Strategie generieren.")
 st.button("🌀 Tipps generieren")
-st.page_link("pages/TippGenerator.py", label="➡️ Weiter zum Tipp Generator")
