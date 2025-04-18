2fd89ccbf6186244300c0c69395ac31c7d5d7f08
GamblingStar2025 <newspoolestate@gmail.com>
2025-04-18 22:56:28 +0200

    Add files via upload

--- utils/auth.py
+++ utils/auth.py
@@ -1,7 +1,1 @@
-
-import streamlit as st
-
-def require_login():
-    if "email" not in st.session_state:
-        st.warning("Bitte logge dich ein, um fortzufahren.")
-        st.stop()
+# Auth utilities
